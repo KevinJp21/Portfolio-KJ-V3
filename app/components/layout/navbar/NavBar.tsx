7c8c2b483be6370e7cc63df07a024d0679eda534
KevinJp21 <kevinjp821@gmail.com>
2025-05-07 16:55:19 -0500

    add links and icon to navbar

--- app/components/layout/navbar/NavBar.tsx
+++ app/components/layout/navbar/NavBar.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import ThemeSwitch from "@/components/ThemeSwitch";
+import Link from "next/link";
 export default function NavBar() {
   const [scroll, setScroll] = useState(false);
 
@@ -17,9 +18,24 @@ export default function NavBar() {
     <header className="h-14">
       <nav className="h-full w-full flex items-center justify-center py-4">
         <div className="h-full w-[90%] flex items-center justify-around px-3">
-          <h1 className="text-[var(--color-Grey-Dark)]">Kevin Julio Pineda</h1>
+          <h1 className="text-base text-[var(--Grey-Dark)] font-bold">Kevin Julio Pineda</h1>
           <div>
-            <ul>
+            <ul className="m-0 p-0 list-none flex items-center [&>*]:px-2.5 [&>*_a]:text-[var(--Grey)] [&>*_a]:transition-normal [&>*_a]:duration-300 [&>*_a]:ease-out [&>*_a]:text-sm [&>*_a]:font-bold [&>*_a]:hover:text-[var(--Blue-Hover)]">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/projects">Project</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/skills">Skills</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
               <li>
                 <ThemeSwitch />
               </li>
