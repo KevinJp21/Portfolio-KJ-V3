fadd6796e2f2a0ffc7d9d784dda5bde017fd8983
KevinJp21 <kevinjp821@gmail.com>
2025-05-13 17:27:38 -0500

    implement i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import { Metadata } from "next";
-import Layout from "./components/layout/Layout";
-import Hero from "./sections/Home/Hero/Hero";
+import Layout from "@/components/layout/Layout";
+import Hero from "@/sections/Home/Hero/Hero";
+
 export const metadata: Metadata = {
     title: "Portfolio | Kevin Julio",
     description: "Soy Kevin Julio Pineda (KevinJp21), ingeniero de sistemas especializado en desarrollo Full-Stack Descubre mis trabajos y habilidades en mi portfolio.",
   };
   
-export default function Home() {
+export default async function Home() {
   return (
     <Layout>
       <Hero />
--- app/sections/Home/Hero/Hero.tsx
+++ app/sections/Home/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import { useTranslations } from "next-intl";
+
 export default function Hero() {
+    const t = useTranslations("HomePage");
     return(
         <div>
-            <h1>Hero</h1>
+            <h1>{t("title")}</h1>
         </div>
     )
 }
