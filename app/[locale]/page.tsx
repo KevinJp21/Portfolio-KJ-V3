302bccfcc10f39c6861fbd0b24c99e1391ba261d
KevinJp21 <kevinjp821@gmail.com>
2025-05-14 17:11:57 -0500

    feat: Implement dynamic metadata generation and add i18n support to the homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,42 @@
-import { Metadata } from "next";
 import Layout from "@/components/layout/Layout";
 import Hero from "@/sections/Home/Hero/Hero";
+import { AbstractIntlMessages } from "next-intl";
+import { getMessages } from "next-intl/server";
 
-export const metadata: Metadata = {
-    title: "Portfolio | Kevin Julio",
-    description: "Soy Kevin Julio Pineda (KevinJp21), ingeniero de sistemas especializado en desarrollo Full-Stack Descubre mis trabajos y habilidades en mi portfolio.",
+interface Messages extends AbstractIntlMessages {
+  Home: {
+    metaData: {
+      title: string;
+      description: string;
+    };
   };
+}
+
+export async function generateMetadata({params}: {params: Promise<{locale: string}>;}) {
+  const { locale } = await params;
+  const messages = await getMessages({locale}) as Messages;
+  const title = messages.Home.metaData?.title;
+  const description = messages.Home.metaData?.description;
+  
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: "https://kevinjp.dev/",
+      languages: {
+        "en": "https://kevinjp.dev/en",
+        "es": "https://kevinjp.dev/es",
+      },
+    },
+    openGraph: {
+      title,
+      description,
+      images: [
+        {url: "https://kevinjp.dev/og.png"},
+      ],
+    },
+  }
+}
   
 export default async function Home() {
   return (
