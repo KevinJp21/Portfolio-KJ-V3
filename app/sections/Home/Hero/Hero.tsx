a115984d5f1c7dccb02c371a0147a4c04a6b3fed
KevinJp21 <kevinjp821@gmail.com>
2025-05-15 13:02:53 -0500

    .

--- app/sections/Home/Hero/Hero.tsx
+++ app/sections/Home/Hero/Hero.tsx
@@ -1,13 +1,14 @@
 import { useTranslations } from "next-intl";
 import Markdown from "react-markdown";
-import { Link } from "../../../../i18n/navigation";
+import Link from "next/link";
+/*En produccion no sale ni la imagen ni el pdf */
 export default function Hero() {
     const t = useTranslations();
     return(
         <section className="min-h-[37.5rem] flex items-center justify-center py-8 px-5 mx-auto max-w-[90rem] w-full">
             <div className="w-full h-full flex justify-center items-center lg:gap-10 flex-wrap p-5">
                 <div className="relative flex justify-center items-center h-[25rem] w-[25rem] [background:var(--imgBG)] [clip-path:circle()]">
-                  <img src="assets/Images/KevinJp.avif" alt="Kevin Jp" className="w-full h-full object-contain " />
+                  <img src="/assets/Images/KevinJp.avif" alt="Kevin Jp" className="w-full h-full object-contain " />
                 </div>
                 <div className="max-w-[35rem] w-full">
                     <h2 className="text-4xl md:text-5xl  text-[var(--Grey-Dark)] text-center font-semibold p-1 leading-15 text-balance">
@@ -19,9 +20,9 @@ export default function Hero() {
                     </Markdown>
                     </div>
                     <div className="flex justify-center gap-2.5 mt-2.5">
-                        <Link href="assets/Docs/KevinJpCV.pdf" className="flex justify-center items-center w-[8.875rem] h-10 rounded-4xl cursor-pointer bg-[var(--Blue)] text-[var(--White)] font-semibold text-center text-sm hover:bg-[var(--Blue-Hover)] transition-all duration-300">
+                        <a href="/assets/Docs/KevinJpCV.pdf" className="flex justify-center items-center w-[8.875rem] h-10 rounded-4xl cursor-pointer bg-[var(--Blue)] text-[var(--White)] font-semibold text-center text-sm hover:bg-[var(--Blue-Hover)] transition-all duration-300">
                             {t("Home.btnCV")}
-                        </Link>
+                        </a>
                         <Link href="mailto:kevinjp21@gmail.com" className="flex justify-center items-center w-[8.875rem] h-10 rounded-4xl cursor-pointer bg-[var(--Blue)] text-[var(--White)] font-semibold text-center text-sm hover:bg-[var(--Blue-Hover)] transition-all duration-300">
                             {t("Home.btnContact")}
                         </Link>
