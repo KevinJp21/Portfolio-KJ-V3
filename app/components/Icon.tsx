ee7956961b2989427651029769ac8939ba526612
KevinJp21 <kevinjp821@gmail.com>
2025-06-20 18:28:39 -0500

    update filter name

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -6,7 +6,7 @@ type IconProps = {
   export default function Icon({ name, className}: IconProps) {
     return (
       <svg className={className}>
-        <use href={`/assets/Icons/Icons.svg?${Date.now()}#${name}`} />
+        <use href={`/assets/Icons/Icons.svg#${name}`} />
       </svg>
     );
   }
